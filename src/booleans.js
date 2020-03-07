daa326f3f3971b33c05e1626fed9eb629d540b41
Aniko Veiszhab <veiszhab.aniko@gmail.com>
2020-03-07 23:01:58 +0000

    Booleans completed

--- src/booleans.js
+++ src/booleans.js
@@ -1,61 +1,69 @@
 const negate = a => {
-  // your code here
+  return !a;
 };
 
 const both = (a, b) => {
-  // your code here
+  return a&&b;
 };
 
 const either = (a, b) => {
-  // your code here
+  return a||b;
 };
 
 const none = (a, b) => {
-  // your code here
+  return !a&&!b;
 };
 
 const one = (a, b) => {
-  // your code here
+  return a !== b;
 };
 
 const truthiness = a => {
-  // your code here
+  return !!a;
 };
 
 const isEqual = (a, b) => {
-  // your code here
+  return( a === b);
 };
 
 const isGreaterThan = (a, b) => {
-  // your code here
+  return (a > b);
 };
 
 const isLessThanOrEqualTo = (a, b) => {
-  // your code here
+  return (a <= b);
 };
 
 const isOdd = a => {
-  // your code here
+  return !!(a % 2);
 };
 
 const isEven = a => {
-  // your code here
+  return a % 2 === 0;
 };
 
 const isSquare = a => {
-  // your code here
+  return Math.sqrt(a) % 1 === 0;  
 };
 
 const startsWith = (char, string) => {
-  // your code here
+  return char === string[0]; 
 };
 
 const containsVowels = string => {
-  // your code here
-};
+  let input = string.toLowerCase();
+  const vowels = ['a','e','i','o','u'];
+  const resultArray = [];
+    for (let i = 0; i < input.length; i++) {
+      for (let j = 0; j < vowels.length; j++) {
+        if(input[i] === vowels[j]){resultArray.push(input[i])}
+    }
+  }
+  return !!resultArray.length;
+}
 
 const isLowerCase = string => {
-  // your code here
+  return string.charAt(0).toLowerCase() === string.charAt(0);
 };
 
 module.exports = {
